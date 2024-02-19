8db4526df04619731020bff54e2e7af9aeb70227
Azeltor <151580496+Azeltor@users.noreply.github.com>
2024-02-19 11:20:48 +0100

    Ajout Class Bouton

---
+++ Fonctions/menu.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+
+
+class Bouton:
+
+  def __init__(self, x, y, image, scale):
+    width = image.get_width()
+    height = image.get_height()
+    self.image = pygame.transform.scale(
+        image, (int(width * scale), int(height * scale)))
+    self.rect = self.image.get_rect()  # Rectangle autour de l'image
+    self.rect.topleft = (x - width / 2, y)
+
+  def draw(self, surface):
+    surface.blit(self.image, (self.rect.x, self.rect.y))
