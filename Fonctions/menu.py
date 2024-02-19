362acbccff4888ced051a25417b462e369669927
Azeltor <151580496+Azeltor@users.noreply.github.com>
2024-02-19 14:00:02 +0100

    bouton orga

--- Fonctions/menu.py
+++ Fonctions/menu.py
@@ -13,14 +13,14 @@ pygame.display.set_caption('Test Jeu')
 screen = pygame.display.set_mode((screen_width - 10, screen_height - 50),pygame.RESIZABLE)
 
 '''Chargement Des Images'''
-Imagefond = pygame.image.load('Graphisme\Bg.jpg')
+Imagefond = pygame.image.load('Graphisme\Background\Bg.jpg')
 Imagefond = pygame.transform.scale(Imagefond, (screen_width, screen_height))
 QuitterNoir = pygame.image.load('Graphisme\Boutons\Bouton_Quitter_Noir-removebg-preview.png')
-#QuitterOrange = pygame.image.load('asset\Bouton_Quitter_Orange-removebg-preview.png')
-#OptionOrange = pygame.image.load('asset\Bouton_Options_Orange-removebg-preview.png')
+QuitterOrange = pygame.image.load('Graphisme\Boutons\Bouton_Quitter_Orange-removebg-preview.png')
+OptionOrange = pygame.image.load('Graphisme\Boutons\Bouton_Options_Orange-removebg-preview.png')
 OptionsNoir = pygame.image.load('Graphisme\Boutons\Bouton_Options_Noir-removebg-preview.png')
 PlayNoir = pygame.image.load('Graphisme\Boutons\Bouton_Jouer_Noir-removebg-preview.png')
-#PlayOrange = pygame.image.load('asset\Bouton_Jouer_Orange-removebg-preview.png')
+PlayOrange = pygame.image.load('Graphisme\Boutons\Bouton_Jouer_Orange-removebg-preview.png')
 Iconeimg = pygame.image.load('Graphisme\Boutons\icone.png')
 
 #Time fps
@@ -32,19 +32,10 @@ Quitter = Button(screen_width / 2, screen_height / 2, QuitterNoir, 1)  #Bouton q
 Option = Button(screen_width / 2, screen_height / 2 - 150, OptionsNoir,1)  #Bouton Option
 Icone = Button(screen_width - 100, screen_height / 100, Iconeimg,1)  #Bouton pour mettre en fullscreen
 Play = Button(screen_width / 2, screen_height / 2-300, PlayNoir,1)  #Bouton pour jouer
-
+QuitterO = Button(screen_width / 2, screen_height / 2, QuitterOrange, 1)
+OptionO = Button(screen_width / 2, screen_height / 2-150, OptionOrange, 1)
+playO = Button(screen_width / 2, screen_height / 2-300, PlayOrange, 1)
 #Action en cas de clic
-def check_play_button(mouse_pos):
-  """Vérifie si le bouton de lecture a été cliqué."""
-  if Play.rect.collidepoint(mouse_pos):
-    # Si le clic est dans la zone du bouton, quitter le jeu
-    pygame.quit()
-  if Icone.rect.collidepoint(mouse_pos):
-    pygame.display.toggle_fullscreen()
-  if Option.rect.collidepoint(mouse_pos):
-    pygame.quit()
-  if Quitter.rect.collidepoint(mouse_pos):
-    pygame.quit()
 
 
 # Boucle De jeu
@@ -58,10 +49,11 @@ while running:
     if event.type == pygame.QUIT:  # Si le joueur veut quitter la fenêtre
       running = False
       print('Le jeu se ferme')
-    elif event.type == pygame.MOUSEBUTTONUP:  # Si un clic de souris est détecté
-      if event.button == 1:  # Si le clic est le bouton gauche de la souris
-        check_play_button(event.pos)  # Vérifie si le bouton de lecture a été cliqué
-
+    if Quitter.draw(screen):
+       pygame.quit()
+    if Icone.rect.collidepoint(pygame.mouse.get_pos()) and event.type == pygame.MOUSEBUTTONUP:  # Si un clic de souris est détecté
+      if event.button == 1:
+        pygame.display.toggle_fullscreen()
   # Dessiner les boutons
   Play.draw(screen)
   Icone.draw(screen)
