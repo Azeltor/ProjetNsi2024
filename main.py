f707ef9d7750dd3bca048d5bc4ce06e67e654547
Minitruuck <151580939+Minitruuck@users.noreply.github.com>
2024-02-25 16:33:27 +0100

    map grotte

---
+++ main.py
@@ -0,0 +1,15 @@
+'''Fichier Pour La Conception Du Menu'''
+import pygame, os
+from classes.Bouton import Button
+from Constantes import constante_partie as cp
+from Fonctions import jeu
+
+
+
+'''Création Fenêtre Pour menu'''
+pygame.init()
+jeu.fenetre()
+jeu.menu()
+
+
+
---
+++ maps/tileset_nsi2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="32" tileheight="32" tilecount="3800" columns="40">
+ <image source="tileset (1).png" width="1280" height="3052"/>
+</tileset>
