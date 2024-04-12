e4b1c57067da3d510e05ee57fde652a1e2020532
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-04-12 11:42:28 +0200

    Update Vie.py

--- classes/Vie.py
+++ classes/Vie.py
@@ -1,8 +1,19 @@
 import pygame
-class vie:
-  max_health = 100
-  health = 20
-  ratio = health / max_health
+class bar_de_vie():
+  def __init__(self, x, y, w, h, max_hp):
+    self.x = x
+    self.y = y
+    self.w = w
+    self.h = h
+    self.hp = max_hp
+    self.max_hp = max_hp
 
-  pygame.draw.rect(screen, "red", (250, 250, 300, 40))
-  pygame.draw.rect(screen, "green", (250, 250, 300 * ratio, 40))
+  def draw(self, surface):
+    #calcule le ratio de vie
+    ratio = self.hp / self.max_hp
+    pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
+    pygame.draw.rect(surface, "green", (self.x, self.y, self.w * ratio, self.h))
+
+
+vie_joueur = bar_de_vie(250, 200, 300, 40, 100)
+
