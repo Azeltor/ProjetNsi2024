010057efe2482ce787fb5091960773a3e67490aa
Gabriel LABOUREAU <gabriel.laboureau@lbes-hugo.etab.local>
2024-05-13 11:49:53 +0200

    push map grotte

---
+++ maps/tileset_test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_test" tilewidth="32" tileheight="32" tilecount="3800" columns="40">
+ <image source="tileset_test.png" width="1280" height="3052"/>
+</tileset>
