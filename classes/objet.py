cf9b80910eae80b6fae7eee0081f870aa5bec862
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-17 10:56:10 +0200

    Update objet.py

--- classes/objet.py
+++ classes/objet.py
@@ -1,8 +1,42 @@
-class Objet:
-    def __init__(self, nom, description):
+class Object:
+    def __init__(self, nom):
         self.nom = nom
+        
+    def ramasser_objet(self, objet):
+        if pressed[pygame.K_e]:
+            self.inventaire.append(objet)
+        
+    def possede_objet(self, nom_objet):
+        for objet in self.inventaire:
+            if objet.nom == nom_objet:
+                return True
+        return False
+    
 
-    def 
+
+        
+Epee = Object("Epée")
+Bouclier = Object("Boucier")
+Chaussures = Object("Chaussure")
+bague = Object("bague")
+stylo = Object("stylo")
+arc = Objet("Arc")
+fleches = Objet("Flèches")
+amulette = Objet("Amulette")
+potionsoins = Objet("Potion de soin")
+tunique = Objet("Tunique")
+grimoire = Objet("Grimoire")
+heaume = Objet("Heaume")
+gants = Objet("Gants")
+lanterne = Objet("Lanterne")
+sacoche = Objet("Sacoche")
+parchemin = Objet("Parchemin")
+clef = Objet("Clef")
+herbes = Objet("Herbes médicinales")
+pain = Objet("Pain")
+fromage = Objet("Fromage")
+trophée = Objet("Trophée")
+
 
     
 
@@ -17,17 +51,6 @@ class Objet:
   # Ajouter dans la classe joueur:
 # self.inventaire = []
  # Méthodes pour gérer l'inventaire et les quêtes
-    def ramasser_objet(self, objet):
-        self.inventaire.append(objet)
-        print(f"{self.nom} a ramassé {objet.nom}.")
+   
 
-    def possede_objet(self, nom_objet):
-        for objet in self.inventaire:
-            if objet.nom == nom_objet:
-                return True
-        return False
 
-    def afficher_inventaire(self):
-        print(f"Inventaire de {self.nom}:")
-        for objet in self.inventaire:
-            print(f" - {objet}")
