61c1409f9bcd250a7a410c6346aba13f3599f670
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-17 10:41:42 +0200

    Update objet.py

--- classes/objet.py
+++ classes/objet.py
@@ -1,11 +1,10 @@
 class Objet:
     def __init__(self, nom, description):
         self.nom = nom
-        self.description = description
 
-    def __str__(self):
-        return f"{self.nom}: {self.description}"
-      
+    
+
+    
 
 
 
