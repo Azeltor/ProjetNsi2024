bedbcf435ceda8963ba4f66670c34f674b2a621b
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-17 11:14:07 +0200

    Update quete.py

--- classes/quete.py
+++ classes/quete.py
@@ -7,9 +7,18 @@ class Quete:
 
 
 
-    def proposer_quete(self,joueur):
+    def proposer_quete(self,joueur,Npc):
+        if self.quete_actuelle == None:
+            si tu parle a ce pnj la :
+                attribue cette que la spécialement 
+            
+            
+
+            
+            
       si il a pas de quete alors , proposer la quete qui sera stocker dans un dico 
       ex : quete1, quete2 , quete3 , quete4 ---> vont être créer a la main , sont dans la classe objet
+        Puis on va devoir cherche le coffre , qui va contenir l'object que la quete demande
       
 
     def verifier_completion(self, joueur):
@@ -24,6 +33,30 @@ class Quete:
 
     def __str__(self):
         return f"Quête: {self.titre}\nDescription: {self.description}\nObjets requis: {', '.join(self.objets_requis)}"
+
+# Création des quêtes : 
+quete1 = Quete("La quête du chevalier", "Trouver l'épée et le bouclier.", ["Épée", "Bouclier"])
+quete2 = Quete("La quête de l'archer", "Trouver l'arc et les flèches.", ["Arc", "Flèches"])
+quete3 = Quete("La quête du mage", "Trouver le grimoire et l'amulette.", ["Grimoire", "Amulette"])
+quete4 = Quete("La quête du guérisseur", "Trouver les herbes médicinales et la potion de soin.", ["Herbes médicinales", "Potion de soin"])
+quete5 = Quete("La quête de l'aventurier", "Trouver la tunique et le heaume.", ["Tunique", "Heaume"])
+quete6 = Quete("La quête du voleur", "Trouver les gants et la lanterne.", ["Gants", "Lanterne"])
+quete7 = Quete("La quête de l'explorateur", "Trouver la sacoche et le parchemin.", ["Sacoche", "Parchemin"])
+quete8 = Quete("La quête du gardien", "Trouver la clef et le trophée.", ["Clef", "Trophée"])
+quete9 = Quete("La quête du cuisinier", "Trouver le pain et le fromage.", ["Pain", "Fromage"])
+
+# Dictionnaire des quêtes disponibles
+quetes_disponibles = {
+    "quete1": quete1,
+    "quete2": quete2,
+    "quete3": quete3,
+    "quete4": quete4,
+    "quete5": quete5,
+    "quete6": quete6,
+    "quete7": quete7,
+    "quete8": quete8,
+    "quete9": quete9 }
+
 
 # A ajouter dans la classe joueur : self.
 
