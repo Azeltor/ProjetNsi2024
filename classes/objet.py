6e036eb07cff99da4ac5730a242adec0a06e7a7b
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-17 10:37:18 +0200

    Update objet.py

--- classes/objet.py
+++ classes/objet.py
@@ -1,6 +1,32 @@
-import pygame
-class num_item:
-  def __init__(self):
-    pass
+class Objet:
+    def __init__(self, nom, description):
+        self.nom = nom
+        self.description = description
 
-items: dict[str,num_item] = {
+    def __str__(self):
+        return f"{self.nom}: {self.description}"
+      
+
+
+
+  
+  
+  
+  
+  # Ajouter dans la classe joueur:
+# self.inventaire = []
+ # Méthodes pour gérer l'inventaire et les quêtes
+    def ramasser_objet(self, objet):
+        self.inventaire.append(objet)
+        print(f"{self.nom} a ramassé {objet.nom}.")
+
+    def possede_objet(self, nom_objet):
+        for objet in self.inventaire:
+            if objet.nom == nom_objet:
+                return True
+        return False
+
+    def afficher_inventaire(self):
+        print(f"Inventaire de {self.nom}:")
+        for objet in self.inventaire:
+            print(f" - {objet}")
