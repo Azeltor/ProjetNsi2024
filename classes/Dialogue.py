1311dfb7e1439187d7add70244f23ee471281808
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-21 16:52:56 +0200

    Update Dialogue.py

--- classes/Dialogue.py
+++ classes/Dialogue.py
@@ -4,6 +4,9 @@ class boite_dialogue(self):
   
 def __init__(self):
   self.boite = pygame.image.load('ProjetNsi2024/Graphisme/Objet/dialog_box.png')
+  self.font = pygame.font.Font('ProjetNsi2024/Graphisme/Objet/dialog_font.ttf')
+  self.text = ""
+  self.visible = False
 
 def afficher(self, ecran):
   ecran.blit(self.boite,(0,0))
