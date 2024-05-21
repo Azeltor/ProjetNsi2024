5159ac252fdb3d80cdd46a8287065d6980ed5a1c
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-21 18:14:09 +0200

    Update Dialogue.py

--- classes/Dialogue.py
+++ classes/Dialogue.py
@@ -6,7 +6,7 @@ Y_POSITION = 470
   
 def __init__(self):
   self.box = pygame. image.Load('/dialogs/dialog_box.png')
-  self.box = pygame. transform.scale(self.dialogue, (700, 100))
+  self.ecran = pygame. transform.scale(self.dialogue, (700, 100))
   self.texts = ["Salut ça va ?", "moi super bien", "bonne aventure"]
   self.text_index = 0
   self.font = pygame.font.Font("../dialogs/dialog_font.ttf", 18)
@@ -14,9 +14,9 @@ def __init__(self):
   
 def render(self, screen):
   if self.etat:
-  screen.blit(self.boite_dialogue,(self.X_POSITION,self.Y_POSITION))
-  text = self.font.render(self.texts[self.text_index],False,(0, 0, 0))
-  screen.blit(text,(self.X_POSITION + 60,self.Y_POSITION + 30))
+    screen.blit(self.boite_dialogue,(self.X_POSITION,self.Y_POSITION))
+    text = self.font.render(self.texts[self.text_index],False,(0, 0, 0))
+    screen.blit(text,(self.X_POSITION + 60,self.Y_POSITION + 30))
 
 
 def prochain_text(self):
