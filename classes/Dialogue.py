8954737384ac373647e545bf1a565981b917b084
Respecte le don petit <162690719+Asuchiase@users.noreply.github.com>
2024-05-21 16:47:36 +0200

    Update Dialogue.py

--- classes/Dialogue.py
+++ classes/Dialogue.py
@@ -1,1 +1,11 @@
+import pytmx, pygame, sys
 
+class boite_dialogue(self):
+  
+def __init__(self):
+  self.boite = pygame.image.load('ProjetNsi2024/Graphisme/Objet/dialog_box.png')
+
+def afficher(self, ecran):
+  ecran.blit(self.boite,(0,0))
+  
+  
