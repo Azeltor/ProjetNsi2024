94b44b70c5062ff62b2ea79ab9f1b3c856b6299b
Azeltor <evanmydu@gmail.com>
2024-05-22 19:17:59 +0200

    Anim pour que pnj ne bouge pas

--- classes/Game.py
+++ classes/Game.py
@@ -18,7 +18,7 @@ class Game:
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, (cp.screen_width, cp.screen_height))
         map_layer.zoom = 2
-        self.npc = [NPC("robin_rouge", 2, Quete("La quête du chevalier", "Trouver l'épée et le bouclier.", ["Épée", "Bouclier"]))]
+        self.npc = [NPC("robin_rouge", 1, Quete("La quête du chevalier", "Trouver l'épée et le bouclier.", ["Épée", "Bouclier"])), NPC("robin_vert", 11, []), NPC("robin_violet", 3, []), NPC("robin_jaune", 3, [])]
         self.player = Player()  #50 * 32, 80* 32
         self.teleport_npcs(self.map)
         self.walls = []
@@ -154,7 +154,7 @@ class Game:
         
 
 
-        self.npc = [NPC("robin", 2,  Quete("La quête du chevalier", "Trouver l'épée et le bouclier.", ["Épée", "Bouclier"]))]
+        self.npc = [NPC("robin_rouge", 1, Quete("La quête du chevalier", "Trouver l'épée et le bouclier.", ["Épée", "Bouclier"])), NPC("robin_vert", 11, []), NPC("robin_violet", 3, []), NPC("robin_jaune", 3, [])]
         self.teleport_npcs(self.map)
         for pnj in self.npc:
             self.group.add(pnj)
